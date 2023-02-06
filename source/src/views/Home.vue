cc39e0457dce255b66247823ad2cf294276e61b5
Exis <91220554+exis9@users.noreply.github.com>
2023-02-06 20:02:14 +0900

    Add files via upload

---
+++ source/src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+	<TheGame/>
+	<router-view/>
+</template>
+
+<script lang="ts">
+	import { defineComponent } from 'vue';
+	import TheGame from '@/components/TheGame.vue'; // @ is an alias to /src
+	
+	export default defineComponent({
+		name: 'Home',
+		components: {
+			TheGame,
+		},
+	});
+</script>
