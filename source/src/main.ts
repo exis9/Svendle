6b315dc538d575a79a2d7705c480e0feb2257161
Exis <91220554+exis9@users.noreply.github.com>
2023-02-06 19:53:39 +0900

    Delete source directory

--- source/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import PrimeVue from 'primevue/config';
-import ToastService from 'primevue/toastservice';
-import ConfirmationService from 'primevue/confirmationservice';
-
-createApp(App).use(router).use(PrimeVue, { ripple: true }).use(ToastService).use(ConfirmationService).mount('#app')
